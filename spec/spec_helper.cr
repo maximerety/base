a677b52c8fb7fe8b4311d89d5b17345b77c373f2
maximerety <maxime.rety@gmail.com>
2022-12-26 19:08:59 +0100

    feat: add main script

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,2 @@
 require "spec"
-require "../src/base"
+require "../src/base/*"
--- src/base.cr
+++ src/base.cr
@@ -1,6 +1,37 @@
-# TODO: Write documentation for `Base`
-module Base
-  VERSION = "0.1.0"
+require "./base/*"
 
-  # TODO: Put your code here
+module Base
+  def self.run!
+    newline = false
+    opts = Options.parse!(CONVERTERS.keys)
+
+    while input = gets
+      input = input.chomp
+
+      if input.empty?
+        newline = false
+        next
+      end
+
+      input_converter = CONVERTERS[opts.input_base][:from]
+      binary = input_converter.call(input)
+
+      if !opts.output_base.empty?
+        output_converter = CONVERTERS[opts.output_base][:to]
+        output = output_converter.call(binary, opts)
+        print "#{newline ? "\n" : ""}#{output}"
+      else
+        print "#{newline ? "\n" : ""}base#{opts.input_base} input: #{input}"
+        CONVERTERS.each do |base, converter|
+          output_converter = converter[:to]
+          output = output_converter.call(binary, opts)
+          print "\n  -> base#{base}: #{output}"
+        end
+      end
+
+      newline = true
+    end
+  end
 end
+
+Base.run!
